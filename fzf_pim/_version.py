3c30a7f8f83a4ef52251cf2fd6b884d577f01a13
Jorgen V <mail@jvik.no>
2026-06-24 13:12:24 +0200

    Revert version to '0.1.dev8+gc6716c178.d20260624' and update version tuple accordingly

--- fzf_pim/_version.py
+++ fzf_pim/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.1.1.dev0+g55a4ac2e8.d20260624'
-__version_tuple__ = version_tuple = (0, 1, 1, 'dev0', 'g55a4ac2e8.d20260624')
+__version__ = version = '0.1.dev8+gc6716c178.d20260624'
+__version_tuple__ = version_tuple = (0, 1, 'dev8', 'gc6716c178.d20260624')
 
 __commit_id__ = commit_id = None
